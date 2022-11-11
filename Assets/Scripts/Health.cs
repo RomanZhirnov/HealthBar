36a51ea73dc11587df67b14dd5dbab212e9394a5
RomanZhirnov <rvzh@mail.ru>
2022-11-11 15:47:55 +0300

    Innitial

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Health : MonoBehaviour
+{
+    private float _currentHealth;
+    private float _maxHealth;
+    private float _minHealth;
+    private float _damage;
+    private float _treat;
+    private Coroutine _moveHealthBar;
+
+    [SerializeField] private Slider _healthSlider;
+    [SerializeField] private TMP_Text _text;
+    [SerializeField] private Gradient _gradient;
+    [SerializeField] private Image _fill;
+    [SerializeField] private float _changingSpeed = 0.1f;
+
+    private void Start()
+    {
+        _currentHealth = 50f;
+        _maxHealth = 100f;
+        _minHealth = 0f;
+        _damage = 10f;
+        _treat = 10f;
+
+        _healthSlider.maxValue = _maxHealth;
+        _healthSlider.minValue = _minHealth;
+        _healthSlider.value = _currentHealth;
+
+        _moveHealthBar = StartCoroutine(MoveBar(_maxHealth));
+    }
+
+    public void IncreaseHealth()
+    {
+        float targetHealt = _currentHealth + _treat;
+
+        if (targetHealt > _maxHealth)
+        {
+            targetHealt = _maxHealth;
+        }
+
+        if (_moveHealthBar != null)
+        {
+             StopCoroutine(_moveHealthBar);
+        }
+
+        _moveHealthBar = StartCoroutine(MoveBar(targetHealt));
+
+        ChangeText(_currentHealth);
+    }
+
+    public void ReduceHealth()
+    {
+        float targetHealt = _currentHealth - _damage;
+
+        if (targetHealt < _minHealth)
+        {
+            targetHealt = _minHealth;
+        }
+
+        if (_moveHealthBar != null && targetHealt != _currentHealth)
+        {
+            StopCoroutine(_moveHealthBar);
+        }
+
+        _moveHealthBar = StartCoroutine(MoveBar(targetHealt));
+
+        ChangeText(_currentHealth);
+    }
+
+    private void ChangeText(float health)
+    {
+        int IntHealth = (int)health;
+        _text.text = IntHealth.ToString();
+    }
+
+    private IEnumerator MoveBar(float targetHealth)
+    {
+        while (_currentHealth != targetHealth)
+        {
+            _currentHealth = Mathf.MoveTowards(_currentHealth, targetHealth, _changingSpeed);
+            _healthSlider.value = _currentHealth;
+            _fill.color = _gradient.Evaluate(_healthSlider.normalizedValue);
+            ChangeText(_currentHealth);
+            yield return null;
+        }
+    }
+}
